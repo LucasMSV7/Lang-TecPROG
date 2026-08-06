f0397f894a559a4e0e66d2b6856959f1bf9b144c
LucasMSV7 <cassisversuti@gmail.com>
2026-08-06 11:32:28 -0300

    aula1

---
+++ aula1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int a,b,c;
+	
+	a = 8;
+	b = 19;
+	c = a+b;
+	
+	printf("A soma de %d + %d = %d", a,b,c);
+	
+	return 0;
+}
