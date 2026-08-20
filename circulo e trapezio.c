78d278e1c05e5c26c7c9b3dc4aa7b5b21004ee70
LucasMSV7 <cassisversuti@gmail.com>
2026-08-20 10:36:08 -0300

    atividade

---
+++ circulo e trapezio.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define pi 3.141592
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+  float area, raio, r2;
+  
+  printf("Insira o raio do circulo: ");
+  scanf("%f", &raio);
+  
+  r2 = raio*raio;
+  area = pi*r2;
+  
+  printf("A area do circulo de raio %f = %f", raio, area);	
+	
+	return 0;
+}
