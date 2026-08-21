8c6be43a2645e711c0f684e5b9247e9ec2d0100e
LucasMSV7 <cassisversuti@gmail.com>
2026-08-21 17:28:08 -0300

    Update lista de atividade 2.c

--- lista de atividade 2.c
+++ lista de atividade 2.c
@@ -4,6 +4,52 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+
+//exercicio1//
+
+	int idade, ano_Atual, ano_Nascimento;
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite o ano atual: ");
+    scanf("%d", &ano_Atual);
+
+    ano_Nascimento = ano_Atual - idade;
+
+    printf("Ano de nascimento: %d\n", ano_Nascimento);
+
+
+//exercicio2//
+
+	double K, M;
+
+    printf("Digite a velocidade em km/h: ");
+    scanf("%lf", &K);
+
+    M = K / 3.6;
+
+    printf("Velocidade em m/s: %.2lf\n", M);
+
+
+//exercicio3//
+
+	double real, cotacao, dolar;
+
+    printf("Digite o valor em real: ");
+    scanf("%lf", &real);
+
+    printf("Digite a cotacao do dolar: ");
+    scanf("%lf", &cotacao);
+
+    dolar = real/cotacao;
+
+    printf("Valor em dolar: %.2lf\n", dolar);
+
+
+//exercicio4//
+
+	
 	
 //exercicio10//
 	
