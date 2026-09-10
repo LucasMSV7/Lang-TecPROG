b8b4be55c294263ea36bf3923b791ae9c2037ad3
LucasMSV7 <cassisversuti@gmail.com>
2026-09-10 09:47:24 -0300

    Correct variable name and fix calculation errors
    Fixed variable name from 'multiDigito' to 'multDigito' and corrected the calculation of 'restoII'.

--- atividadeC.c
+++ atividadeC.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
     printf("Confirme o cpf %d%d%d.%d%d%d.%d%d%d-%d%d", 
 		dg1, dg2, dg3, dg4, dg5, dg6, dg7, dg8, dg9, dgv, dgv2);
 		
-	soma = multiDigito(dg1,10)+multDigito(dg2,9)+multDigito(dg3,8)+
+	soma = multDigito(dg1,10)+multDigito(dg2,9)+multDigito(dg3,8)+
 		    multDigito(dg4,7)+multDigito(dg5,6)+multDigito(dg6,5)+
 		    multDigito(dg7,4)+multDigito(dg8,3)+multDigito(dg9,2);
 		
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
 	if (resto == 10) resto = 0;
 	printf("\n%d",resto);
 	
-	soma = multiDigito(dg1,11)+multDigito(dg2,10)+multDigito(dg3,9)+
+	soma = multDigito(dg1,11)+multDigito(dg2,10)+multDigito(dg3,9)+
 		    multDigito(dg4,8)+multDigito(dg5,7)+multDigito(dg6,6)+
 		    multDigito(dg7,5)+multDigito(dg8,4)+multDigito(dg9,3)+multDigito(dgv,2);
 	
